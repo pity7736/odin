7a359c7d0be98869af35c03d8ac38c1a40d4958b
Julián Cortés <julian.cortes77@pm.me>
2022-03-30 00:00:13 -0500

    fix: typo

--- tests/unit_tests/respositories/test_expense_repository.py
+++ tests/unit_tests/respositories/test_expense_repository.py
@@ -3,8 +3,8 @@ from odin.repositories import ExpenseRepository
 
 def test_get_expense_by_uuid(expense_fixture):
     repository = ExpenseRepository()
-    got_expense = repository.get_by(uuid=expense_fixture.uuid)
+    gotten_expense = repository.get_by(uuid=expense_fixture.uuid)
 
-    assert got_expense.date == expense_fixture.date
-    assert got_expense.amount == expense_fixture.amount
-    assert got_expense.uuid == expense_fixture.uuid
+    assert gotten_expense.date == expense_fixture.date
+    assert gotten_expense.amount == expense_fixture.amount
+    assert gotten_expense.uuid == expense_fixture.uuid
--- tests/unit_tests/test_expense_getter.py
+++ tests/unit_tests/test_expense_getter.py
@@ -3,7 +3,7 @@ from odin.controllers import ExpenseGetter
 
 def test_get_expense_by_uuid(expense_fixture):
     expense_getter = ExpenseGetter()
-    got_expense = expense_getter.get_by_uuid(uuid=expense_fixture.uuid)
+    gotten_expense = expense_getter.get_by_uuid(uuid=expense_fixture.uuid)
 
-    assert got_expense.date == expense_fixture.date
-    assert got_expense.amount == expense_fixture.amount
+    assert gotten_expense.date == expense_fixture.date
+    assert gotten_expense.amount == expense_fixture.amount
