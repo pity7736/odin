a24b56b07ba2a0a8158f66d67083293662c6569d
Julián Cortés <julian.cortes77@pm.me>
2022-07-28 22:32:14 -0500

    feat: add expenses to internal list

--- odin/models/wallet.py
+++ odin/models/wallet.py
@@ -9,11 +9,14 @@ class Wallet(Entity):
     _balance = fields.DecimalField(required=True)
     _name = fields.StrField(required=True)
     _uuid = fields.StrField(required=True)
-    _expenses = fields.ListField()
+    _expenses: list[Expense] = fields.ListField()
 
     def __init__(self, **kwargs):
         kwargs.setdefault('uuid', uuid.uuid4())
+        kwargs.setdefault('expenses', [])
         super().__init__(**kwargs)
 
     def add_expense(self, expense: Expense):
+        assert isinstance(expense, Expense)
         self._balance -= expense.amount
+        self._expenses.append(expense)
---
+++ tests/unit_tests/models/test_wallet.py
@@ -0,0 +1,46 @@
+import datetime
+from decimal import Decimal
+
+from pytest import raises
+
+from odin.repositories import WalletRepository
+from tests.factories import WalletBuilder, ExpenseFactory
+
+
+def test_assert_is_expense_instance(category_fixture):
+    wallet = WalletBuilder().build()
+    with raises(AssertionError):
+        wallet.add_expense(Decimal('100_000'))
+
+
+def test_add_expense(category_fixture):
+    wallet = WalletBuilder().build()
+    expense = ExpenseFactory.create(
+        date=datetime.date.today(),
+        amount='100_000',
+        category=category_fixture,
+    )
+    wallet.add_expense(expense)
+
+    assert wallet.balance == Decimal('900_000')
+    assert wallet.expenses == [expense]
+
+
+def test_get_wallet_with_expenses_from_repository_and_add_expense(category_fixture):
+    wallet = WalletBuilder().build()
+    expense = ExpenseFactory.create(
+        date=datetime.date.today(),
+        amount='100_000',
+        category=category_fixture,
+    )
+    wallet.add_expense(expense)
+    wallet = WalletRepository().get_by_name(name=wallet.name)
+    expense = ExpenseFactory.create(
+        date=datetime.date.today(),
+        amount='100_000',
+        category=category_fixture,
+    )
+    wallet.add_expense(expense)
+
+    assert wallet.balance == Decimal('800_000')
+    assert len(wallet.expenses) == 2
