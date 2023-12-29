8b4c5c5d06d83029700cd69c94a8dbf685b8b04b
Julián Cortés <julian.cortes77@protonmail.com>
2023-12-28 23:20:26 -0500

    refactor: remove edgedb repositories

--- odin/accounting/repositories/repository_factory.py
+++ odin/accounting/repositories/repository_factory.py
@@ -1,25 +1,14 @@
-from odin import settings
 from .in_memory_reposotiries import InMemoryCategoryRepository, InMemoryWalletRepository, InMemoryTransferRepository
-from .edgedb_repositories import EdgeDBCategoryRepository, EdgeDBWalletRepository, EdgeDBTransferRepository
 from .repositories import CategoryRepository, WalletRepository, TransferRepository
 
 
 def get_category_repository() -> CategoryRepository:
-    if settings.REPOSITORY == 'in-memory':
-        return InMemoryCategoryRepository()
-    elif settings.REPOSITORY == 'edgedb':
-        return EdgeDBCategoryRepository()
+    return InMemoryCategoryRepository()
 
 
 def get_wallet_repository() -> WalletRepository:
-    if settings.REPOSITORY == 'in-memory':
-        return InMemoryWalletRepository()
-    elif settings.REPOSITORY == 'edgedb':
-        return EdgeDBWalletRepository()
+    return InMemoryWalletRepository()
 
 
 def get_transfer_repository() -> TransferRepository:
-    if settings.REPOSITORY == 'in-memory':
-        return InMemoryTransferRepository()
-    elif settings.REPOSITORY == 'edgedb':
-        return EdgeDBTransferRepository()
+    return InMemoryTransferRepository()
